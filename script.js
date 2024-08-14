d569aa822ef624535feb8126413624d027e4a0c0
Poorva khatri <poorvakhatri04@gmail.com>
2024-08-14 12:37:07 +0530

    Now we can generate and download the qr code

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const downloadBtn=document.getElementById("downloadBtn");
 const qrContainer=document.querySelector('.qr-body');
 
 let size=sizes.value;
+sizes.addEventListener('change',(e)=>{
+    size=e.target.value;
+    isEmptyInput();
+})
 generateBtn.addEventListener('click',(e)=>{
     e.preventDefault();
-    generateQRCode();
+    isEmptyInput();
 });
+function isEmptyInput(){
+    qrText.value.length>0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");
+}
 function generateQRCode(){
     qrContainer.innerHTML="";
     new QRCode(qrContainer,{
@@ -19,3 +26,13 @@ function generateQRCode(){
         colorDark:"#000",
     });
 }
+downloadBtn.addEventListener('click',()=>{
+    let img=documnet.quesrySelector('.qr-body img');
+    if(img!==null){
+        let imgAttr=img.getAtrribute('src');
+        downloadBtn.setAttribute("href",imgAttr);
+    }
+    else{
+        downloadBtn.setAttribute("href",`${document.querySelector('canvas').toDataURL()}`);
+    }
+});
